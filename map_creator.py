6b7dede3ad7b336a260f9e1dd96c80500efc3508
supplient <437566830@qq.com>
2018-12-10 11:41:57 +0800

    Refactor MapCreator.

--- map_creator.py
+++ map_creator.py
@@ -1,10 +1,37 @@
-from Zoom import largeMap
 from mymap import Map
+
+class MapCreator:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        self.new_map = [[" "] * self.width for i in range(self.length)]
+
+    # sign 为o/#/|/_这样的符号 只能画直线
+    def draw(self, start, end, sign):
+        if start[0] == end[0]:
+            for i in range(start[1], end[1]+1):
+                self.new_map[start[0]][i] = sign
+        else:
+            if start[1] == end[1]:
+                for i in range(start[0], end[0]+1):
+                    self.new_map[i][start[1]] = sign
+            else:
+                return None
+
+    def output(self, path):
+        line = list()
+        for i in range(self.length):
+            for t in range(self.width):
+                line.append(self.new_map[i][t])
+            if i != self.length-1:
+                line.append("\n")
+        with open(path, "wt") as fd:
+            fd.write("".join(line))
 
 def createBasicLargeMap():
     size = 100
     half_size = int(10/2)
-    large_map = largeMap(size, size, "data/large_basic_map.txt")
+    large_map = MapCreator(size, size)
     large_map.draw((0,0), (size-1,0), Map.mark_target)
     large_map.draw((0,1), (size-1,1), Map.mark_target)
     large_map.draw((0,2), (half_size-1,2), Map.mark_wall)
@@ -12,7 +39,18 @@ def createBasicLargeMap():
     large_map.draw((0,3), (0,size-1), Map.mark_wall)
     large_map.draw((size-1,3), (size-1,size-1), Map.mark_wall)
     large_map.draw((0,size-1), (size-1,size-1), Map.mark_wall)
-    large_map.output()
+    large_map.output("data/large_basic_map.txt")
+
+def createExampleMap():
+    largemap = MapCreator(1000,1000)
+    largemap.draw((0,0),(0,999),"o")
+    largemap.draw((1,0),(999,0),"#")
+    largemap.draw((1,999), (999, 999), "#")
+    largemap.draw((999, 0), (999, 999), "#")
+    largemap.draw((1, 0), (1, 300), "#")
+    largemap.draw((1, 700), (1, 999), "#")
+    largemap.output("data/largeMap")
 
 if __name__ == "__main__":
     createBasicLargeMap()
+    createExampleMap()
